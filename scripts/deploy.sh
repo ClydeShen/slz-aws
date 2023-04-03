9d54f644f600baf44889d35cc93eb6d25132628b
Clyde Shen <clshen@deloitte.com>
2023-04-04 10:51:40 +1200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,3 +10,6 @@ ZIP_FILE_NAME="slz-lambda-fns.zip"
 # upload the zip file to S3
 AWS_SDK_LOAD_CONFIG=1 aws s3 cp build/$ZIP_FILE_NAME s3://$BUCKET_NAME/$ZIP_FILE_NAME --region $REGION
 
+# update the Lambda functions using Terraform
+# terraform init
+# terraform apply
